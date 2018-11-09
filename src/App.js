f3c74aea5b26a1089cd6424716e6173fb218bd35
saussayjesica <jesicagreenwell@gmail.com>
2018-11-10 07:55:28 +1100

    feat: added fake fetch

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './App.css';
 import Header from './components/Header';
 import MenuItem from './components/MenuItem';
 import Order from './components/Order';
+import Loading from './components/Loading';
 import { menuData } from './data/menuData';
 
 class App extends Component {
   state = {
     order: [],
-    total: 0
+    total: 0,
+    data: []
   };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ data: menuData });
+    }, 3000);
+  }
 
   handleUpdate = (name, price) => {
     const newItem = { name, price };
-    this.setState(({ order, total }) => ({
-      order: [...order, newItem],
-      total: total + price
-    }));
+    // this.setState(({ order, total }) => ({
+    //   order: [...order, newItem],
+    //   total: total + price
+    // }));
+    this.setState({
+      order: this.state.order.concat(newItem),
+      total: this.state.total + price
+    });
   };
 
   resetOrder = () => {
@@ -27,39 +39,44 @@ class App extends Component {
   };
 
   submitOrder = () => {
-    var synth = window.speechSynthesis;
-    var utterThis = new SpeechSynthesisUtterance(
+    const synth = window.speechSynthesis;
+    const utterThis = new SpeechSynthesisUtterance(
       `Welcome to emoji eats. You have ordered ${this.state.order.map(
         item => item.name
-      )}. This come to a total of ${this.state.total} dollars`
+      )}. This comes to a total of ${this.state.total} dollars`
     );
     synth.speak(utterThis);
   };
 
   render() {
-    const { order, total } = this.state;
+    const { order, total, data } = this.state;
     return (
       <div className="App">
         <Header />
-        <div className="menu">
-          <div className="menuitems">
-            {menuData.map(item => (
-              <MenuItem
-                updateOrder={this.handleUpdate}
-                emoji={item.emoji}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-              />
-            ))}
+        {data.length > 0 ? (
+          <div className="menu">
+            <div className="menuitems">
+              {data.map((item, index) => (
+                <MenuItem
+                  key={index}
+                  updateOrder={this.handleUpdate}
+                  emoji={item.emoji}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                />
+              ))}
+            </div>
+            <Order
+              order={order}
+              total={total}
+              handleResetClick={this.resetOrder}
+              handleSubmitClick={this.submitOrder}
+            />
           </div>
-          <Order
-            order={order}
-            total={total}
-            handleResetClick={this.resetOrder}
-            handleSubmitClick={this.submitOrder}
-          />
-        </div>
+        ) : (
+          <Loading />
+        )}
       </div>
     );
   }
